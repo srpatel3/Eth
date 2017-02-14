36fc4284e9d77e4261333ea77890f9c4c5977e8a
srpatel3 <srpatel3@go.olemiss.edu>
2017-02-14 15:45:42 -0600

    Feb 14

--- src/Mark2/Mark2.sol
+++ src/Mark2/Mark2.sol
@@ -1,18 +1,21 @@
 contract Mark2{
     uint counter;
     struct list{
-        string typo;
-        address addr;
-        bool exists;
+        string typo;  // name of dataset
+        string keyword;
+        address addr; // seller address
+        bool exists;  // does this bucket contain valid data?
     }
     mapping (string => list) _list;
     struct node{
         address add;
-        address next;
+        address next;  // can next == 0x00000000000000000000000000  ?
         address head;
     }
 
     mapping (string =>mapping(address=>node)) _node;
+
+    address head = 0x0000;  //???
 
     function Mark2(){
         _node['root'][0].add = 1;
@@ -21,6 +24,7 @@ contract Mark2{
     }
 
     function add(string _typo){
+
         if(!_list[_typo].exists){
             _list[_typo].typo = _typo;
             _list[_typo].exists = true;
@@ -28,9 +32,55 @@ contract Mark2{
             counter++;
         }
         else{
+            // here we add a new list node. Maybe also check
+            // for a matching existing list node?
             counter--;
         }
     }
+
+
+#     function addbyDSName(node n){
+#
+#         if(!_list[n.typo].exists){
+#             _list[n.typo].typo = _keyword;
+#             _list[n.typo].exists = true;
+#             _list[n.typo].addr = 0;
+#             counter++;
+#         }
+#         else{
+#             // here we add a new list node. Maybe also check
+#             // for a matching existing list node?
+#             counter--;
+#         }
+#     }
+
+
+// not needed yet
+#     function addbyKeyword(node n){
+#
+#         if(!_list[n.keyword].exists){
+#             _list[n.keyword].typo = _keyword;
+#             _list[n.keyword].exists = true;
+#             _list[n.keyword].addr = 0;
+#             counter++;
+#         }
+#         else{
+#             // here we add a new list node. Maybe also check
+#             // for a matching existing list node?
+#             counter--;
+#         }
+#     }
+
+
+    function lookupByDSName(string dsname) constant returns(node []) {
+
+    }
+
+// not needed yet
+#     function lookupByKeyWord(string keyword) constant returns(node []) {
+#
+#     }
+
     function getCounter() constant returns(uint){
         return counter;
     }
