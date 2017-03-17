8d1adbf8fa463b6abf55a85823490143ef1e6b5b
srpatel3 <srpatel3@go.olemiss.edu>
2017-03-17 04:07:02 -0500

    COnditions added to Mark1

--- src/Final_Phase/Mark1/Mark1.sol
+++ src/Final_Phase/Mark1/Mark1.sol
@@ -3,8 +3,9 @@ contract X{
     
     //structure for maintainig the list of datasetNames and the last element in the list
     struct datasetList{
-        string datasetName;
-        address listHead;
+        string datasetName; //Name of the dataset
+        address listHead; //Will point to the latest node added in the list
+        bool isExist;
         //address 
     }
     
@@ -19,6 +20,8 @@ contract X{
     }
     
     //Actual information of sellers will be stored in this lisr
+    //Should I be having two structs or single one will work?
+    
     struct sellerList{
         string sellerName;
         address sellerAddress;
@@ -27,6 +30,7 @@ contract X{
         string datasetLocation;
         string datasetDescription;
         uint cost;
+        bool isExist;
         uint datasetFeedBack;
         uint counterForGoodFeedBack;
         uint counterForBadFeedBack;
@@ -46,24 +50,64 @@ contract X{
     mapping (address=>sellerAccounts) list_Accounts;
     
     //you should be knowing what to so Shirish!!! Suck it up.
-    function makeNewDataset(string _datasetName){
-        list_Dataset[_datasetName].datasetName = _datasetName;
-        list_Dataset[_datasetName].listHead = 0;
+    function makeNewDatasetType(string _datasetName) returns (string){
+        if(list_Dataset[_datasetName].isExist != true){
+            list_Dataset[_datasetName].datasetName = _datasetName;
+            list_Dataset[_datasetName].listHead = 0;
+            list_Dataset[_datasetName].isExist = true;
+            return "Dataset type successfully created.";
+        }
+        else{
+            return "Dataset type already exist.";
+        }
     }
     
-    function addNewDataset(string _datasetName, string _sellerName, string _IP, string _datasetLocation, string _datasetDescription, uint _cost){
-        list_Seller[_datasetName][msg.sender].sellerName = _sellerName;
-        list_Seller[_datasetName][msg.sender].sellerAddress = msg.sender;
-        list_Seller[_datasetName][msg.sender].IP = _IP;
-        list_Seller[_datasetName][msg.sender].datasetLocation = _datasetLocation;
-        list_Seller[_datasetName][msg.sender].datasetDescription = _datasetDescription;
-        list_Seller[_datasetName][msg.sender].cost = _cost;
-        list_Seller[_datasetName][msg.sender].next = list_Dataset[_datasetName].listHead;
-        list_Seller[_datasetName][msg.sender].counterForGoodFeedBack = 0;
-        list_Seller[_datasetName][msg.sender].counterForBadFeedBack = 0;
-        list_Seller[_datasetName][msg.sender].datasetFeedBack = 0;
-        list_Dataset[_datasetName].listHead = msg.sender;
-        list_Accounts[msg.sender].accountBalance += 50;
+    function modifyDataset(string _datasetName, string _sellerName, string _IP, string _datasetLocation, string _datasetDescription, uint _cost) returns (string){
+        if(list_Seller[_datasetName][msg.sender].isExist == true){
+            list_Seller[_datasetName][msg.sender].sellerName = _sellerName;
+            list_Seller[_datasetName][msg.sender].sellerAddress = msg.sender;
+            list_Seller[_datasetName][msg.sender].IP = _IP;
+            list_Seller[_datasetName][msg.sender].datasetLocation = _datasetLocation;
+            list_Seller[_datasetName][msg.sender].datasetDescription = _datasetDescription;
+            list_Seller[_datasetName][msg.sender].cost = _cost;
+            list_Seller[_datasetName][msg.sender].next = list_Dataset[_datasetName].listHead;
+            list_Seller[_datasetName][msg.sender].counterForGoodFeedBack = 0;
+            list_Seller[_datasetName][msg.sender].counterForBadFeedBack = 0;
+            list_Seller[_datasetName][msg.sender].datasetFeedBack = 0;
+            list_Dataset[_datasetName].listHead = msg.sender;
+            list_Accounts[msg.sender].accountBalance += 50;
+            return "Dataset values updated";
+        }
+        
+        else{
+            return "Dataset is not available you can add new dataset using function addNewDataset.";
+        }
+    }
+    function addNewDataset(string _datasetName, string _sellerName, string _IP, string _datasetLocation, string _datasetDescription, uint _cost) returns (string){
+        if(list_Dataset[_datasetName].isExist == true){
+            if(list_Seller[_datasetName][msg.sender].isExist!=true){
+                list_Seller[_datasetName][msg.sender].sellerName = _sellerName;
+                list_Seller[_datasetName][msg.sender].sellerAddress = msg.sender;
+                list_Seller[_datasetName][msg.sender].IP = _IP;
+                list_Seller[_datasetName][msg.sender].datasetLocation = _datasetLocation;
+                list_Seller[_datasetName][msg.sender].datasetDescription = _datasetDescription;
+                list_Seller[_datasetName][msg.sender].cost = _cost;
+                list_Seller[_datasetName][msg.sender].isExist = true;
+                list_Seller[_datasetName][msg.sender].next = list_Dataset[_datasetName].listHead;
+                list_Seller[_datasetName][msg.sender].counterForGoodFeedBack = 0;
+                list_Seller[_datasetName][msg.sender].counterForBadFeedBack = 0;
+                list_Seller[_datasetName][msg.sender].datasetFeedBack = 0;
+                list_Dataset[_datasetName].listHead = msg.sender;
+                list_Accounts[msg.sender].accountBalance += 50;
+                return "Dataset successfully added";
+            }
+            else{
+                return "Dataset already available if you want you can modify it using modifyDataset.";
+            }
+        }
+        else{
+         return "Wrong Dataset Type. You can add New dataset type using function makeNewDataset.";   
+        }
     }
     
     function buyInformation(string _datasetName, address _sellerAddress) constant returns(string){
@@ -78,16 +122,28 @@ contract X{
     }
     
     
-    function giveGoodFeed(string _datasetName, address _sellerAddress){
-        list_Seller[_datasetName][_sellerAddress].datasetFeedBack += 1;
-        list_Seller[_datasetName][_sellerAddress].list_goodFeed[list_Seller[_datasetName][_sellerAddress].counterForGoodFeedBack].Address = msg.sender;
-        list_Seller[_datasetName][_sellerAddress].counterForGoodFeedBack += 1;
+    function giveGoodFeed(string _datasetName, address _sellerAddress) returns (string){
+        if(list_Seller[_datasetName][_sellerAddress].isExist == true){
+            list_Seller[_datasetName][_sellerAddress].datasetFeedBack += 1;
+            list_Seller[_datasetName][_sellerAddress].list_goodFeed[list_Seller[_datasetName][_sellerAddress].counterForGoodFeedBack].Address = msg.sender;
+            list_Seller[_datasetName][_sellerAddress].counterForGoodFeedBack += 1;
+            return "Feedback successfully given.";
+        }
+        else{
+            return "Something went wrong, you may want to check credentials again.";
+        }
     }
     
-    function giveBadFeed(string _datasetName, address _sellerAddress){
-        list_Seller[_datasetName][_sellerAddress].datasetFeedBack -= 1;
-        list_Seller[_datasetName][_sellerAddress].list_badFeed[list_Seller[_datasetName][_sellerAddress].counterForBadFeedBack].Address = msg.sender;
-        list_Seller[_datasetName][_sellerAddress].counterForBadFeedBack += 1;
+    function giveBadFeed(string _datasetName, address _sellerAddress)returns (string){
+        if(list_Seller[_datasetName][_sellerAddress].isExist == true){
+            list_Seller[_datasetName][_sellerAddress].datasetFeedBack -= 1;
+            list_Seller[_datasetName][_sellerAddress].list_badFeed[list_Seller[_datasetName][_sellerAddress].counterForBadFeedBack].Address = msg.sender;
+            list_Seller[_datasetName][_sellerAddress].counterForBadFeedBack += 1;
+            return "Feedback Successfully given.";
+        }
+        else{
+            return "Something went wrong, you may want to check credentials again.";
+        }
     }
 }
 
@@ -103,6 +159,7 @@ list_Seller[_datasetName][msg.sender].next);
         return (list_Dataset[_datasetName].datasetName,list_Dataset[_datasetName].listHead);
     }
 }
+
 
 
 
