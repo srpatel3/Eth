b751c7b8687cd8af06c06bccaaaf82b7b4441b5c
srpatel3 <srpatel3@go.olemiss.edu>
2017-04-03 20:38:41 -0500

    tests added

--- Scripts/mine.js
+++ Scripts/mine.js
@@ -5,3 +5,5 @@ web3.eth.defaultAccount = eth.accounts[0]
 miner.start()
 
 loadScript("/home/shirish/Eth/Scripts/Compiled")
+//loadScript("/home/shirish/Eth/Scripts/test.js")
+//console.log("this is console output" + y.address)
--- Scripts/setter.js
+++ Scripts/setter.js
@@ -1,4 +1,4 @@
-//var something = "lol";
+//this is for adding data to the contract definitation
 
 var contractAddress = y.address;
 console.log("this is console output" + contractAddress);
